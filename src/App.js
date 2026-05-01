a87d1d223d35fe93300f48b030200ee84640548c
Muthaiah26 <pandimuthaiah2006@gmail.com>
2026-05-01 12:19:27 +0530

    backend url updated for EC2

--- src/App.js
+++ src/App.js
@@ -744,7 +744,7 @@ const visualizeCode = useCallback(async (currentCode) => {
             code: codeToSend,
         };
 
-        const apiUrl = Process.env.REACT_APP_BACKEND_ENDPOINT; // Ensure this is set in your .env file
+        const apiUrl = `${process.env.REACT_APP_BACKEND_ENDPOINT}`; // Ensure this is set in your .env file
         
         const response = await fetch(apiUrl, {
             method: 'POST',
